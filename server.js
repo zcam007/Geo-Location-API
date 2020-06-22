aee82dcebcc94b5f1a69698d80d1402ee31eba4b
Chandu's <chandu.prince951@gmail.com>
2020-06-22 17:09:10 -0400

    Port change

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var newObj=(obj[rowNumber]["fields"]);
 res.send(newObj)
 })
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
